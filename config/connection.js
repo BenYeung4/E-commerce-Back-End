bedef488ea39b9e5695565ada99d12f05da2a6d0
Ben <byeungis@gmail.com>
2022-07-14 18:34:53 -0400

    minor bugs

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,25 @@
-require('dotenv').config();
+//sets up the secure password and username
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
+//import the Sequelize constructor from the library
+const Sequelize = require("sequelize");
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+//below following code is used after heroku has been confirmed
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
